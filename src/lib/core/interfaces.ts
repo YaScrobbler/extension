b168ea078a8b8adf35e0407a7b2558e730fbfc52
Vladislav <wennerryle@gmail.com>
2025-07-10 07:03:48 +0300

    feat: add initListeners fn

--- src/lib/core/interfaces.ts
+++ src/lib/core/interfaces.ts
@@ -1,12 +1,13 @@
 export interface YaMusicTrackInfo {
     track: string;
     artists: string[];
-    cover: string;
+    cover?: string;
 
     /** duration in ms */
     duration: number;
-    url: string;
+    url?: string;
 
     /** The time the track started playing, in UNIX timestamp format (integer number of seconds since 00:00:00, January 1st 1970 UTC). This must be in the UTC time zone. */
     timestamp: number;
+    chosenByUser?: 1 | 0;
 }
